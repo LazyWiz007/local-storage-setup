489c1943b231d1ab1c81546031e31acd2831e031
LazyWiz007 <parshuramgore007@gmail.com>
2021-10-28 16:17:02 +0530

    first commit

---
+++ src/components/Form1.jsx
@@ -0,0 +1,33 @@
+import { useState,useEffect} from "react";
+
+const Form1 = () => {
+  const [name, setName] =  useState(() => {
+    // getting stored value
+    const saved = localStorage.getItem("name");
+    const initialValue = JSON.parse(saved);
+    return initialValue || "";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("name", JSON.stringify(name))
+}, [name])
+
+
+
+  return (
+    <div>
+    <form>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Full name"
+        aria-label="fullname"
+      />
+       <input type="submit" value="Submit"></input>
+    </form>
+    </div>
+  );
+};
+
+export default Form1;
